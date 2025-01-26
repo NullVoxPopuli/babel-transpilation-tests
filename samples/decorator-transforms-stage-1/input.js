3bb6fee9f606eae19ffae7569456e7835b51c265
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-01-26 11:50:03 -0500

    Better example

--- samples/decorator-transforms-stage-1/input.js
+++ samples/decorator-transforms-stage-1/input.js
@@ -1,5 +1,12 @@
 class Demo {
+  static {
+    hi(this, there())
+  }
+
   @tracked foo = 0;
 
   inc = () => this.foo++;
+
+  #private() { }
+  #privateProperty = 0;
 }
--- samples/decorator-transforms-stage-1/output.js
+++ samples/decorator-transforms-stage-1/output.js
@@ -1,10 +1,15 @@
 import { g, i } from "decorator-transforms/runtime";
 class Demo {
   static {
+    hi(this, there());
+  }
+  static {
     g(this.prototype, "foo", [tracked], function () {
       return 0;
     });
   }
   #foo = (i(this, "foo"), void 0);
   inc = () => this.foo++;
+  #private() {}
+  #privateProperty = 0;
 }
